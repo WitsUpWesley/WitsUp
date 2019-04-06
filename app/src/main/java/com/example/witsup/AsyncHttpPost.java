3ce458abf76d61cb649e998f73f276f482e098a3
WitsUpWesley <1619159@students.wits.ac.za>
2019-04-06 16:21:34 +0200

    Added login

---
+++ app/src/main/java/com/example/witsup/AsyncHttpPost.java
@@ -0,0 +1,76 @@
+package com.example.witsup;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.SQLOutput;
+
+/**
+ * Created by 1657041 on 2018/04/26.
+ */
+
+public abstract class AsyncHttpPost extends AsyncTask<String, String, String> {
+    String address;
+    ContentValues parameters;
+    public AsyncHttpPost(String address, ContentValues parameters) {
+        this.address = address;
+        this.parameters = parameters;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            System.out.println("-------------------------------------"+parameters);
+            URL url = new URL(address);
+            HttpURLConnection connection =(HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            connection.setDoOutput(true);
+
+            connection.setDoInput(true);
+
+            if (parameters.size() > 0) {
+                Uri.Builder builder = new Uri.Builder();
+
+                for (String s : parameters.keySet()) {
+
+                    builder.appendQueryParameter(s,parameters.getAsString(s));
+                }
+
+                String query = builder.build().getEncodedQuery();
+
+                OutputStream os;
+
+                os= connection.getOutputStream();
+
+
+
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String response = br.readLine();
+
+            br.close();
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "[]";
+        }
+    }
+    @Override
+    protected abstract void onPostExecute(String output);
+}
--- app/src/main/java/com/example/witsup/MainActivity.java
+++ app/src/main/java/com/example/witsup/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.witsup;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,207 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Login");
+        TextView textView =  textView=(TextView) findViewById(R.id.btnCreateNewuser1);
+        textView.setVisibility(View.INVISIBLE);
+        textView.setEnabled(false);
+
+
+    }
+    public void login(View v){
+        EditText t;
+        final TextView uName = findViewById(R.id.txtUsername);
+
+        final TextView LABEL = findViewById(R.id.lblOutput);
+
+        String username, password;
+
+        t=(EditText)findViewById(R.id.txtUsername);
+        username= t.getText().toString();
+        t=(EditText)findViewById(R.id.txtPassword);
+        password= t.getText().toString();
+
+        ContentValues params = new ContentValues();
+
+        params.clear();
+        params.put( "Username", username);
+        params.put( "Password", password);
+
+AsyncHttpPost asyncHttpPost = new AsyncHttpPost("http://lamp.ms.wits.ac.za/~s1355485/loginTest.php", params) {
+   //   AsyncHttpPost asyncHttpPost = new AsyncHttpPost("http://s1355485@lamp.ms.wits.ac.za/loginTest.php", params) {
+
+            @Override
+            protected void onPostExecute(String output) {
+                String checker="";
+                int start, end;
+
+                if(output.equals("[]")){
+                    System.out.println("User doesn't exist");
+                    LABEL.setText("Invalid Credentials"+output);
+
+                }else {//logged in
+//
+                 //   start = output.indexOf(":")+2;
+                 //   end= output.indexOf(34,start);
+//
+                 //   checker= output.substring(start,end);
+
+                    /*
+                    System.out.println("username:"+checker);
+
+
+                    if (!LABEL.getText().toString().equals(checker)) {
+                        System.out.println("wrong password");
+                        LABEL.setText("WRONG PASSS" + output);
+                    } else {
+                        System.out.println("logged in");
+                        LABEL.setText("R" + output);
+                    }
+                    */
+                    LABEL.setText("Logged in"+output);
+
+                    //temp star
+                   // Intent intent = new Intent(MainActivity.this, homepageOnClicks.class);
+                  //  intent.putExtra("username", checker);
+                 //   startActivity(intent); ***
+
+
+
+                }
+            }
+        };
+        asyncHttpPost.execute();
+
+    }
+    public void createAccount(View v){
+        EditText t;
+        String username, password;
+
+        t=(EditText)findViewById(R.id.txtUsername);
+        username= t.getText().toString();
+        t=(EditText)findViewById(R.id.txtPassword);
+        password= t.getText().toString();
+
+        TextView textView = (TextView) findViewById(R.id.lblConfirmPassword);
+        textView.setVisibility(View.VISIBLE);
+        textView=(TextView) findViewById(R.id.txtConfirmPassword);
+        textView.setVisibility(View.VISIBLE);
+        textView=(TextView) findViewById(R.id.btnLogin);
+        textView.setVisibility(View.INVISIBLE);
+        textView.setEnabled(false);
+
+
+        textView=(TextView) findViewById(R.id.btnCreateUser);
+        textView.setVisibility(View.INVISIBLE);
+        textView.setEnabled(false);
+        textView=(TextView) findViewById(R.id.btnCreateNewuser1);
+        textView.setVisibility(View.VISIBLE);
+        textView.setEnabled(true);
+
+
+
+    }
+    public void checkIfUserExists(View v){
+        System.out.println("Running check if user exists");
+        EditText t;
+        String username, password1,password2;
+        TextView textView ;
+        t=(EditText)findViewById(R.id.txtUsername);
+        username= t.getText().toString();
+        t=(EditText)findViewById(R.id.txtPassword);
+        password1= t.getText().toString();
+        t=(EditText)findViewById(R.id.txtConfirmPassword);
+        password2= t.getText().toString();
+
+
+
+        if(username.equals("")||username.contains(" ")){
+            textView = (TextView) findViewById(R.id.lblOutput);
+            textView.setText("Username cannot be blank/contain spaces");
+            textView.setVisibility(View.VISIBLE);
+            System.out.println("Username Is Incorrect");
+        }
+        else {
+
+            if (password1.equals(password2)) {
+                ContentValues params = new ContentValues();
+
+                params.put("Username",username);
+                AsyncHttpPost asyncHttpPost = new AsyncHttpPost(
+                        "http://lamp.ms.wits.ac.za/~s1355485/checkUser.php",params) {
+                    @Override
+                    protected void onPostExecute(String output) {
+                        if(output.equals("[]")){
+                            //user doesnt exist so can create new user
+                            createNewUser();
+                            System.out.println("Create new user called 1");
+                        }else{
+                            //user exists choose different username
+                            outputToLabel("Username already exists");
+                        }
+
+
+                    }
+                };
+                asyncHttpPost.execute();
+
+                textView = (TextView) findViewById(R.id.lblOutput);
+                textView.setText("Creating a New User");
+
+
+            } else {
+
+                outputToLabel("Passwords do not match");
+
+                System.out.println("Passwords don't match");
+            }
+
+        }
+
+
+    }
+
+    public void outputToLabel(String output){
+        TextView textView = (TextView) findViewById(R.id.lblOutput);
+        textView.setVisibility(View.VISIBLE);
+        textView.setText(output);
+    }
+
+
+
+    public void createNewUser(){
+        TextView t , textView;
+        t=(EditText)findViewById(R.id.txtUsername);
+        final String username= t.getText().toString();
+        t=(EditText)findViewById(R.id.txtPassword);
+        final String  password= t.getText().toString();
+        System.out.println("in create new user");
+
+        ContentValues params = new ContentValues();
+        params.put("Username",username);
+        params.put("Password", password);
+
+
+        AsyncHttpPost asyncHttpPost = new AsyncHttpPost("http://lamp.ms.wits.ac.za/~s1355485/createUser.php",params) {
+            @Override
+            protected void onPostExecute(String output) {
+                System.out.println(output);
+
+            }
+        };
+        asyncHttpPost.execute();
+        textView = (TextView) findViewById(R.id.btnCreateNewuser1);
+        textView.setVisibility(View.INVISIBLE);
+        textView.setEnabled(false);
+
+        textView = (TextView) findViewById(R.id.txtConfirmPassword);
+        textView.setVisibility(View.INVISIBLE);
+        textView = (TextView) findViewById(R.id.lblConfirmPassword);
+        textView.setVisibility(View.INVISIBLE);
+
+
+        textView = (TextView) findViewById(R.id.btnLogin);
+        textView.setVisibility(View.VISIBLE);
+        textView.setEnabled(true);
     }
 }
