bda57c6a82be208bf662a3586f204006bac513e1
WitsUpWesley <1619159@students.wits.ac.za>
2019-05-29 23:07:32 +0200

    @wesley adding unit tests

---
+++ app/src/androidTest/java/com/example/witsup/HomePageTest.java
@@ -0,0 +1,36 @@
+package com.example.witsup;
+
+import android.widget.LinearLayout;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomePageTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+
+    @Ignore("Tested in app that populates all")
+    @Test
+    public void processData() {
+
+    }
+
+
+
+    @Test
+    public void logout() {
+    }
+
+    @Test
+    public void goToCreateCourse() {
+    }
+
+    @Test
+    public void goToEnrolInCourse() {
+    }
+}
