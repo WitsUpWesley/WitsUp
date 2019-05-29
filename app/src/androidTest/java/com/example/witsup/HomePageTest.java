5a0a65fedd30d00691ec2d3cd1d5067968683099
WitsUpSteven <1657041@students.wits.ac.za>
2019-05-30 00:05:07 +0200

    test homepage

---
+++ app/src/androidTest/java/com/example/witsup/HomePageTest.java
@@ -0,0 +1,48 @@
+package com.example.witsup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+
+@RunWith(AndroidJUnit4.class)
+public class HomePageTest {
+    @Rule
+
+    public ActivityTestRule rule=new ActivityTestRule(HomePage.class,true,false);
+
+    @Test
+    public void persSonTest(){
+    String personNumber="a";
+        rule.launchActivity(new Intent().);
+
+        onView(withId(R.id.btnEnrolInsCourse)).check(matches(withText("Enrol In Course")));
+        onView(withId(R.id.btnCreateCourse)).check(matches(withText("Create Course")));
+    }
+}
