76c259fb44c28908c72462ca04b1c6b45ace2a94
WitsUpWesley <1619159@students.wits.ac.za>
2019-05-30 14:20:43 +0200

    @wesley adding unit tests

--- app/src/androidTest/java/com/example/witsup/ResourcesforfbTest.java
+++ app/src/androidTest/java/com/example/witsup/ResourcesforfbTest.java
@@ -1,5 +1,9 @@
 package com.example.witsup;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,20 +12,31 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 public class ResourcesforfbTest {
     @Rule
-    public ActivityTestRule rule=new ActivityTestRule(Resourcesforfb.class,true,false);
-    /*@Test//commented out to see coverage
+    public ActivityTestRule rule = new ActivityTestRule(Resourcesforfb.class, true, false);
+
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(testListActivity.class.getName(), null, false);
+
+    @Test
     public void onCreate() {
         assertNotNull(rule.getActivity().findViewById(R.layout.resources));
-    }*/
+    }
 
     @Test
     public void onActivityResult() {
     }
+
     @Ignore("Called by view")
     @Test
     public void onClick() {
@@ -29,6 +44,22 @@ public class ResourcesforfbTest {
 
     @Test
     public void goToViewUploads() {
+        Bundle b = new Bundle();
+        b.putString("username", "a1");
+        b.putString("course", "test");
+
+        rule.launchActivity(new Intent().putExtras(b));
+
+        assertNotNull(rule.getActivity().findViewById(R.id.btnViewUploads));
+
+        onView(withId(R.id.btnViewUploads)).perform(click());
+
+        Activity secondAct = getInstrumentation().waitForMonitorWithTimeout(monitor, 5000);
+
+        assertNotNull(secondAct);
+        secondAct.finish();
+
+
     }
 
     @Test
